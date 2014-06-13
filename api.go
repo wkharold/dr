3c2fc1aed18dc2bea8bc0f1ab6728ce07f49c297
Ward K Harold <wkharold@gmail.com>
2014-06-12 20:29:50 -0500

    Add handlers for the rest of the API

--- api.go
+++ api.go
@@ -8,3 +8,27 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Docker-Registry-Version", "0.6.0")
 	w.WriteHeader(200)
 }
+
+func layer(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
+
+func json(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
+
+func ancestry(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
+
+func tags(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
+
+func tag(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
+
+func repository(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+}
--- registry.go
+++ registry.go
@@ -8,7 +8,15 @@ type Registry struct {
 
 func New() (*Registry, error) {
 	r := mux.NewRouter()
+
 	v1 := r.PathPrefix("/v1").Subrouter()
 	v1.HandleFunc("/_ping", ping)
+	v1.HandleFunc("/images/{imageid}/layer", layer)
+	v1.HandleFunc("/images/{imageid}/json", json)
+	v1.HandleFunc("/images/{imageid}/ancestry", ancestry)
+	v1.HandleFunc("/repositories/{namespace}/{repository}/tags", tags)
+	v1.HandleFunc("/repositories/{namespace}/{repository}/tags/{tag}", tag)
+	v1.HandleFunc("/repositories/{namespace}/{repository}/", repository)
+
 	return &Registry{r}, nil
 }
